50c607df0bd715f5aad67b0e3541eac13ad8cf98
Ilvondir <komsa.m@o2.pl>
2022-11-20 20:14:19 +0100

    drawing records by year

--- script.r
+++ script.r
@@ -1,5 +1,9 @@
 #install.packages("magrittr")
+#install.packages("DescTools")
+#install.packages("ggplot2")
 library(magrittr)
+library(DescTools)
+library(ggplot2)
 
 ds<-read.csv("datasets/BigmacPrice.csv")
 head(ds, n=10)
@@ -13,3 +17,22 @@ class(ds$local_price)
 class(ds$dollar_ex)
 ds$dollar_ex
 class(ds$dollar_price)
+
+nrow(ds)
+
+recordsByYear<-Year(ds$date) %>%
+  table() %>%
+  data.frame()
+recordsByYear
+
+ggplot(recordsByYear, aes(x=., y=Freq)) + geom_bar(stat="identity",
+                                                   width=0.6,
+                                                   fill="indianred1") + theme_minimal() + labs(title="Number of records by year",
+                                                                                         x="Year", y="Frequetion")
+  ggsave("results/recordsByYear.png",
+         width=600,
+         height=400,
+         bg="white",
+         dpi=72,
+         units="px")
+  
