f83f62ae931f312ca545c0bb1c590da74c40e92e
Ilvondir <komsa.m@o2.pl>
2022-11-24 20:21:09 +0100

    background and roll drawed

--- draw.r
+++ draw.r
@@ -4,10 +4,25 @@ library(draw)
 drawSettings(pageWidth=12, pageHeight=10, units="inches")
 drawPage()
 
+#Background
 drawBox(x=5.9,
         y=4.9,
         width=12.4,
         height=10.4,
-        fillColor="#BD9460")
+        fillColor="#005A00")
+
+#Title
+drawText(6, 9,
+         text="My Bigmac",
+         color="#009A00",
+         face="bold",
+         size=92)
+
+drawBox(6, 2,
+        width=6,
+        height=1,
+        fillColor="#C68958",
+        lineColor="#C68958",
+        radius=0.2)
 
 drawExport("results/Bigmac.png")
