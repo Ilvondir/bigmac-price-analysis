22c1544d3f82f8dc3dcd4b2025fabaa424ad948f
Ilvondir <komsa.m@o2.pl>
2022-11-24 22:37:27 +0100

    upper layer drawed

--- draw.r
+++ draw.r
@@ -1,6 +1,8 @@
 #install.packages("draw")
 library(draw)
 
+??draw
+
 drawSettings(pageWidth=12, pageHeight=10, units="inches")
 drawPage()
 
@@ -32,7 +34,7 @@ drawBox(6, 2.5,
         height=0.05,
         fillColor="green",
         lineColor="green",
-        radius=0.1)
+        radius=0.05)
 
 #Down cheese
 drawBox(6, 2.58,
@@ -43,7 +45,39 @@ drawBox(6, 2.58,
         radius=0.05)
 
 #Down meat
-drawBox(6, 2.92,
+drawBox(6, 2.925,
+        width=5.9,
+        height=0.6,
+        fillColor="#562807",
+        lineColor="#562807",
+        radius=0.2)
+
+#Center roll
+drawBox(6, 3.65,
+        width=6,
+        height=0.85,
+        fillColor="#C68958",
+        lineColor="#C68958",
+        radius=0.2)
+
+#Up lettuce
+drawBox(6, 4.1,
+        width=6.05,
+        height=0.05,
+        fillColor="green",
+        lineColor="green",
+        radius=0.05)
+
+#Up cheese
+drawBox(6, 4.175,
+        width=5.9,
+        height=0.1,
+        fillColor="#FBDB65",
+        lineColor="#FBDB65",
+        radius=0.05)
+
+#Up meat
+drawBox(6, 4.526,
         width=5.9,
         height=0.6,
         fillColor="#562807",
@@ -51,4 +85,3 @@ drawBox(6, 2.92,
         radius=0.2)
 
 drawExport("results/Bigmac.png")
-
