f8de8c36bea44f22348de47491d563ce9c3a5074
Ilvondir <komsa.m@o2.pl>
2022-11-24 17:15:32 +0100

    configure basic options

--- draw.r
+++ draw.r
@@ -1,2 +1,13 @@
 #install.packages("draw")
 library(draw)
+
+drawSettings(pageWidth=12, pageHeight=10, units="inches")
+drawPage()
+
+drawBox(x=5.9,
+        y=4.9,
+        width=12.4,
+        height=10.4,
+        fillColor="#BD9460")
+
+drawExport("results/Bigmac.png")
